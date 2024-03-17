6917ddba1aca2f332e556bf22c176a46d18e7f24
Kaushik Dange <kaushikdange3107@gmail.com>
2024-03-17 18:21:55 +0530

    All three charts to be displayed complete

--- src/app/mychart/mychart.component.ts
+++ src/app/mychart/mychart.component.ts
@@ -9,10 +9,50 @@ Chart.register(...registerables);
 export class MychartComponent implements OnInit {
   ngOnInit(): void {
     this.RenderChart();
+    this.PieChart();
   }
   RenderChart() {
-    const myChart = new Chart('piechart', {
+    const myChart = new Chart('barchart', {
       type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  }
+
+  PieChart() {
+    const myChart = new Chart('piechart', {
+      type: 'pie',
       data: {
         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [
